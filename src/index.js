af1f552bed4fe8f745e027fd11dae304da1f8824
Chris <cs924@kent.ac.uk>
2026-09-25 15:07:25 +0100

    Add toolbar elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import "./style.css";
 
-import { } from "./pages/list.js";
-import { } from "./pages/projects.js";
+import {  } from "./pages/list.js";
+import { loadProjectsPage } from "./pages/projects.js";
 
-const projectsListSidebar = document.querySelector("#projects-list-sidebar")
+const toolbar = document.querySelector("#toolbar");
+const mainContent = document.querySelector("#main-content");
 
+const myProjectsButton = document.querySelector("#projects-button");
+
+function displayPage(pageLoader) {
+    toolbar.replaceChildren();
+    mainContent.replaceChildren();
+    pageLoader();
+}
+
+myProjectsButton.addEventListener("click", () => {
+    displayPage(loadProjectsPage);
+});
+
+displayPage(loadProjectsPage);
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,49 @@
 
-export function loadProject () { 
+export function loadProjectsPage () { 
+    loadToolBar();
+    loadProjects();
+}
+
+
+function loadToolBar () {
+    const toolbar = document.querySelector("#toolbar")
+
+    const newProjectButton = document.createElement("button");
+    newProjectButton.textContent = "New Project";
+    newProjectButton.id = "new-project-button";
+    newProjectButton.addEventListener("click", () => {
+        
+    });
+
+    const searchProjectsInput = document.createElement("input");
+    searchProjectsInput.placeholder = "Search Projects...";
     
+    const sortBySelect = document.createElement("select");
+    sortBySelect.id = "sort-by";
+    ["Due Date", "Alphabetical", "Completion"].forEach((sortOption) => {
+        const option = document.createElement("option");
+        option.textContent = sortOption;
+        option.value = sortOption;
+        sortBySelect.append(option);
+    });
+
+    const sortByLabel = document.createElement("label");
+    sortByLabel.textContent = "Sort By:";
+    sortByLabel.htmlFor = "sort-by";
+
+    const sortGroup = document.createElement("div");
+    sortGroup.id = "sort-group";
+    sortGroup.append(sortByLabel, sortBySelect);
+    
+
+    toolbar.append(
+        newProjectButton, 
+        searchProjectsInput,
+        sortGroup
+    ); 
+}
+
+
+function loadProjects () {
+
 }
